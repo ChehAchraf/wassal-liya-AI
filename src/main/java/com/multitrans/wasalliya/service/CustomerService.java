a5e75ab9f2d95c79747723c05560b857e876ff64
chehachraf <achrafchehboun@outlook.com>
2025-11-06 15:58:33 +0100

    injecting the classes into customer service

--- src/main/java/com/multitrans/wasalliya/service/CustomerService.java
+++ src/main/java/com/multitrans/wasalliya/service/CustomerService.java
@@ -1,5 +1,18 @@
 package com.multitrans.wasalliya.service;
 
-public class CustomerService {
+import com.multitrans.wasalliya.model.mapper.CustomerMapper;
+import com.multitrans.wasalliya.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepo;
+    private final CustomerMapper cMapper;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepo, CustomerMapper cMapper) {
+        this.customerRepo = customerRepo;
+        this.cMapper = cMapper;
+    }
 }
