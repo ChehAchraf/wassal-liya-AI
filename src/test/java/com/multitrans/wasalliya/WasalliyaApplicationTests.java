2d8dd6ce0fdfb11cd4b6bd6937fe1f2955e89dce
chehachraf <achrafchehboun@outlook.com>
2025-11-13 11:16:17 +0100

    add integration test to the app

--- src/test/java/com/multitrans/wasalliya/WasalliyaApplicationTests.java
+++ src/test/java/com/multitrans/wasalliya/WasalliyaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.multitrans.wasalliya;
 
+import com.multitrans.wasalliya.service.TourService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
+@ActiveProfiles("dev")
+@TestPropertySource(properties = {"app.optimizer.strategy=ai"})
 class WasalliyaApplicationTests {
 
+    @Autowired
+    private TourService tourService;
+
 	@Test
-	void contextLoads() {
-	}
+	void testOptimizerStrategyIsCorrectlyInjected(){
+        String optimizerName = tourService.getOptimizerName().toString();
+        System.out.println("the optimizer is working now is " + optimizerName);
+
+        assertNotEquals("NearestNeighbor", optimizerName);
+        assertNotEquals("ClarkeWright", optimizerName);
+    }
 
 }
